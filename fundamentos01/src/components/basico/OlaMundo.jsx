1d4cada1519a96f96503e362e49395e3456ce1d0
vitudominguess <vitorpessoadomingues@hotmail.com>
2021-03-30 16:20:51 +0000

    Aula 01 - Primeiro componente

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+
+    const informacao = 'Primeiro componente em JSX'
+
+    return(
+        <>
+            <h2>Componente Olá Mundo</h2>
+            <p>{ informacao }</p>
+        </>
+    )
+}
